ca952f19c04652fe43d248ecb2af3cd04cdf2c84
kiaruh <nikoz.li@gmail.com>
2021-12-07 21:07:42 -0300

    validaciones hecho

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,43 +1,16 @@
 import { useContext, useState} from "react"
 import { CartData } from "../../context/CartContext"
-import { Button } from '@mui/material'
 import CheckoutItem from "./CheckoutItem"
-import { firestore } from "../../firebase"
-import firebase from 'firebase/app'
+
 import CheckoutForm from "./CheckoutForm"
 
 export const Checkout = () => {
     
-    const {cart, clearCart, total} = useContext(CartData)
-    
+    const {cart, total} = useContext(CartData)
     const [id, setId] = useState('')
     const [ user, setUser ] = useState({name:'', email:'', phone:''})
     const [order, setOrder] = useState({})
 
-    const checkout = () => {
-        
-        const orden = {
-            date: firebase.firestore.Timestamp.now(),
-            buyer: user,
-            items: cart,
-            total: total,
-        }
-        
-        const db = firestore
-        const coleccion = db.collection("ordenes")
-        const query = coleccion.add({orden})
-
-        query
-            .then((resultado) => {
-                setOrder(orden)
-                clearCart()
-                setId(resultado.id)
-            })
-            .catch((error)=>{
-                console.log("ERRORRR: " + error)
-            })
-    }
-    
    if(cart.length === 0 && id === '') {
        return (
            <>
@@ -54,7 +27,7 @@ export const Checkout = () => {
             <p>Nombre: {order.buyer.name}</p>
             <p>Email: {order.buyer.email}</p>
             <p>Telefono: {order.buyer.phone}</p>
-            {order.items.map((e,i) => <p key={e+i}>Producto: {e.name} x {e.qty} subtotal: {e.price * e.qty} </p>)}
+            {order.items.map((e,i) => <p key={e+i}>Producto {i+1}: {e.name} x {e.qty} subtotal: ${e.price * e.qty} </p>)}
             <p>Total: ${order.total}</p>
             <p></p>
             
@@ -63,11 +36,10 @@ export const Checkout = () => {
     return (
         <div>
             <h1>Checkout</h1>
-            <h1>{user.name} {user.phone} {user.email}</h1>
-            <CheckoutForm user={user} setUser={setUser}/>
+            <CheckoutForm user={user} setOrder={setOrder} setId={setId} setUser={setUser}/>
             {cart.map( (e,i)=> <CheckoutItem key={e+i} props={e}/>)}
             <h3>Total a pagar: ${total}</h3>
-            <Button onClick={checkout}>Pagar</Button>
+            
         </div>
     )
 }
--- src/components/checkout/CheckoutForm.js
+++ src/components/checkout/CheckoutForm.js
@@ -1,23 +1,67 @@
-import { useForm } from 'react-hook-form'
+import { firestore } from "../../firebase"
+import firebase from 'firebase/app'
+import { useContext} from "react"
+import { Button } from '@mui/material'
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { CartData } from "../../context/CartContext"
 
+  const schema = yup.object().shape({
+    name: yup.string().required('El nombre es requerido'),
+    email: yup.string().email('Tiene que ser formato email').required('El email es requerido'),
+    phone: yup.string().required('El numero es requerido'),
+  })
 
-function CheckoutForm({setUser, user}){
+function CheckoutForm({setUser, user, setOrder, setId}){
     
-    const { handleSubmit} = useForm()
+    const {cart, clearCart, total} = useContext(CartData)
+    
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(schema),
+      })
 
     const handleInputChange = (event) => {
         setUser({...user, [event.target.name]:event.target.value})
     }
 
-    const onBlur = (data) => {
-        console.log(data)
-    }
+    const checkout = () => {
+        
+        const orden = {
+            date: firebase.firestore.Timestamp.now(),
+            buyer: user,
+            items: cart,
+            total: total,
+        }
+        
+        const db = firestore
+        const coleccion = db.collection("ordenes")
+        const query = coleccion.add({orden})
 
+        query
+            .then((resultado) => {
+                setOrder(orden)
+                clearCart()
+                setId(resultado.id)
+            })
+            .catch((error)=>{
+                console.log("ERRORRR: " + error)
+            })
+    }
+    const onSubmit = (data) => {
+        checkout()
+    }
+  
     return(
-        <form onBlur={handleSubmit(onBlur)}>
-            <input type="text" placeholder='Ingrese Nombre' name='name' onChange={handleInputChange}/>
-            <input type="text" placeholder='Ingrese Email' name='email' onChange={handleInputChange}/>
-            <input type="text" placeholder='Ingrese telefono' name='phone' onChange={handleInputChange}/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+            <input type="text" placeholder='Ingresa tu Nombre' name='name' ref={register} onBlur={handleInputChange} re/>
+            <p>{errors.name?.message}</p>
+            <input type="text" placeholder='Ingresa tu Email' name='email' ref={register} onBlur={handleInputChange}/>
+            <p>{errors.email?.message}</p>
+            <input type="text" placeholder='Ingresa tu telefono' name='phone' ref={register} onBlur={handleInputChange}/>
+            <p>{errors.phone?.message}</p>
+            <Button type="submit">Comprar</Button>
         </form>
     )
 }
