460e12bf6e2ff31aaa2ee5fa1818452d300fe791
kiaruh <nikoz.li@gmail.com>
2023-04-05 17:09:01 -0300

    minor fix

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -1,11 +1,17 @@
+import { Fragment } from "react";
 
-function Footer(){
-
-
-    return (
-
-        <h3>Proyecto Final Coder</h3>
-    )
+function Footer() {
+  return (
+    //add fragment to avoid error
+    <Fragment>
+      <a href="https://www.linkedin.com/in/li-qiyin-339197168/">
+        <h3>Linkedin: Kiaruh</h3>
+      </a>
+      <a href="https://github.com/kiaruh/pickleli-app">
+        <h3>Github: Kiaruh</h3>
+      </a>
+    </Fragment>
+  );
 }
 
-export default Footer
+export default Footer;
